5ecc2389d317e9330513e6495ee489b5a61d4219
Anne van Kesteren <annevk@annevk.nl>
2020-07-15 19:24:59 +0200

    update this to take into account sg decisions

--- review.py
+++ review.py
@@ -3,33 +3,52 @@
 import argparse, datetime, json, os, subprocess, requests
 
 
-def makeGroupsofFive(workstraems):
-    groups = [[]]
+def is_third_monday(d):
+    return d.weekday() == 0 and 15 <= d.day <= 21
+
+def find_monday(d, days=1, limit=10):
     i = 0
-    group = 0
+    while not is_third_monday(d):
+        d = d + datetime.timedelta(days=days)
+        i += 1
+        if i > limit:
+            return None
+    return i * days
 
-    for workstream in workstraems:
+def days_from_third_monday(d):
+    if d.day < 15:
+        return find_monday(d)
+    elif d.day > 21:
+        return find_monday(d, -1)
+    else:
+        forward = find_monday(d)
+        if not forward:
+            return find_monday(d, -1)
+        return forward
+
+
+def find_shortnames(workstreams, month):
+    shortnames = []
+
+    for workstream in workstreams:
         for standard in workstream["standards"]:
-            shortname = href_to_shortname(standard["href"])
-            if i != 0 and i % 5 == 0:
-                groups.append([])
-                group += 1
-            groups[group].append(shortname)
-            i += 1
+            if month not in standard["review_draft_schedule"]:
+                continue
+            shortnames.append(href_to_shortname(standard["href"]))
 
-    return groups
+    return shortnames
 
 def href_to_shortname(href):
     return href[len("https://"):href.index(".")]
 
 
-def maybeCreatePRs(group):
-    for shortname in group:
+def maybe_create_prs(shortnames):
+    for shortname in shortnames:
         os.chdir("../{}".format(shortname))
-        maybeCreatePR(shortname)
+        maybe_create_pr(shortname)
         os.chdir(".")
 
-def maybeCreatePR(shortname):
+def maybe_create_pr(shortname):
     subprocess.run(["git", "checkout", "master"], capture_output=True)
     commits = subprocess.run(["git", "log", "--pretty=oneline", "--abbrev-commit", "--abbrev=8", "--max-count=40"], capture_output=True).stdout
     for commit in commits.split(b"\n"):
@@ -43,7 +62,7 @@ def maybeCreatePR(shortname):
         else:
             break
 
-    # We could consider merging
+    # We should consider merging
     # https://github.com/whatwg/whatwg.org/blob/master/resources.whatwg.org/build/review.sh and
     # https://github.com/whatwg/html/blob/master/review-draft.sh into this script.
     subprocess.run(["make", "review"])
@@ -60,18 +79,30 @@ Under the [WHATWG IPR Policy](https://whatwg.org/ipr-policy), Participants may, 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--group", metavar="N", type=int, choices=range(1,4), required=True)
+    parser.add_argument("--force", dest="force", action="store_true")
     args = parser.parse_args()
 
+    today = datetime.datetime.today()
+    ideal_publication_diff = days_from_third_monday(today)
+
+    if ideal_publication_diff == 0:
+        print("Right on, today is the day!")
+
+    if not args.force:
+        if ideal_publication_diff > 3:
+            print("It's still more than 3 days before publication. Use --force to ignore.")
+            exit(1)
+        elif ideal_publication_diff < -3:
+            print("It's 3 days after publication, hopefully you already published. Use --force to ignore.")
+            exit(1)
+
     db = json.loads(requests.get("https://github.com/whatwg/sg/raw/master/db.json").text)
+    shortnames = find_shortnames(db["workstreams"], today.month)
 
-    groups = makeGroupsofFive(db["workstreams"])
-    # See https://github.com/whatwg/sg/issues/127
-    if len(groups) > 6:
-        print("Please file an issue against whatwg/sg to discuss a new Review Draft publication policy.")
-    if len(groups) > 3:
-        print("Please update the calendar (and this script) to account for more groups.")
+    if len(shortnames) == 0:
+        print("Looks like there's nothing to be published this month.")
+        exit(1)
 
-    maybeCreatePRs(groups[args.group-1])
+    maybe_create_prs(shortnames)
 
 main()
